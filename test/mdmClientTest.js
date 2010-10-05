4f43a172c37fd60b80b5c0dc48263cca601429b3
Rob Tweed <rtweed@mgateway.com>
2010-10-05 08:16:48 +0100

    Changes to license and updating examples

--- test/mdmClientTest.js
+++ test/mdmClientTest.js
@@ -1,12 +1,195 @@
-var mdbmClient = require("./mdbMumpsClient");
 var sys = require("sys");
+var mdbmif = require("./mdbMumpsClient");
 
-var mdbm = new mdbmClient.OperationHelper({
-   mdbId:'rob',
-   mdbSecret:'xxxxx',
+// change endPoint to a different IP address/domain name if GT.M is running on a different server to Node.js
+
+var mdbm = new mdbmif.Client({
+   mdbId:'<your M/DB Id here>',
+   mdbSecret:'<your M/DB Secret key here>',
    endPoint: '127.0.0.1'
 });
 
+
+mdbm.decrement('mdbmTest8', ["this"], 2,
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts('new value: ' + results.value + "\n");
+          }
+   }
+);
+
+/*
+mdbm.getAllSubscripts('mdbmTest7', ["this"],
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts("Subscripts=" + JSON.stringify(results));
+          }
+   }
+);
+*/
+/*
+mdbm.getPreviousSubscript('mdbmTest7', [""],
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts("subscriptValue=" + results.subscriptValue + "\ndataStatus=" + results.dataStatus + "\ndataValue=" + results.dataValue);
+          }
+   }
+);
+*/
+
+/*
+mdbm.getNextSubscript('mdbmTest7', ["this",5],
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts("subscriptValue=" + results.subscriptValue + "\ndataStatus=" + results.dataStatus + "\ndataValue=" + results.dataValue);
+          }
+   }
+);
+*/
+
+/*
+mdbm.kill('mdbmTest7', ["this"],
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts(results.ok + "\n");
+          }
+   }
+);
+*/
+
+/*
+mdbm.getJSON('mdbmTest7',["this"],
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts("GetJSON: " + JSON.stringify(results));
+          }
+   }
+);
+*/
+
+/*
+mdbm.setJSON('mdbmTest7', '', {this:{is:{also:"very cool"}}}, true, 
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts('setJSON: ' + results.ok + "\n");
+          }
+   }
+);
+*/
+
+/*
+mdbm.setJSON('mdbmTest7', ["a","b"], {this:{is:{also:"very cool"}}}, false, 
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts('setJSON: ' + results.ok + "\n");
+          }
+   }
+);
+*/
+
+/*
+mdbm.get('mdbmTest', ["check","this","outxxx"],
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts("dataStatus=" + results.dataStatus + "\nvalue=" + results.value + "\n");
+          }
+   }
+);
+*/
+
+/*
+mdbm.get('mdbmTest', ["check","this","out"],
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts("dataStatus=" + results.dataStatus + "\nvalue=" + results.value + "\n");
+          }
+   }
+);
+*/
+
+/*
+mdbm.get('mdbmTest', '' ,
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts("dataStatus=" + results.dataStatus + "\nvalue=" + results.value + "\n");
+          }
+   }
+);
+*/
+
+/*
+mdbm.set('mdbmTest', '' , "This value should be at the top level",
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts(results.ok + "\n");
+          }
+   }
+);
+*/
+
+/*
+mdbm.set('mdbmTest', ["check","this","out"], "Node.js is so very cool!",
+   function(error, results) {
+          if (error) { 
+             sys.print('Error: ' + error + "\n");
+             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
+          }
+          else {
+            sys.puts(results.ok + "\n");
+          }
+   }
+);
+*/
+
+/*
 mdbm.version( 
    function(error, results) {
           if (error) { 
@@ -18,67 +201,6 @@ mdbm.version(
           }
    }
 );
+*/
 
-
-// This will return an error because subscripts cannot be null
-mdbm.set("mdbmTest", ["check","","out"], "Too cool!",
-   function(error, results) {
-          if (error) { 
-             sys.print('Error: ' + error + "\n");
-             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
-          }
-          else {
-            sys.puts(results.ok + "\n");
-          }
-   }
-);
-
-mdbm.set('mdbmTest', ["check","this","out"], "Too cool!",
-   function(error, results) {
-          if (error) { 
-             sys.print('Error: ' + error + "\n");
-             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
-          }
-          else {
-            sys.puts(results.ok + "\n");
-          }
-   }
-);
-
-mdbm.setJSON('mdbmTest2', {check:{this:{out:"Too cool!"}}}, true,
-   function(error, results) {
-          if (error) { 
-             sys.print('Error: ' + error + "\n");
-             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
-          }
-          else {
-            sys.puts(results.ok + "\n");
-          }
-   }
-);
-
-mdbm.setJSON('mdbmTest3', 
- {'a':111,'b':'hello','arr':['hhjg','lkjklj','uyi'],'arr2':[{'l':'hello','k':'ssss'},{'l':'there','k':'again'}],'d':{'q':'qqqq','r':{'z':123,'x':222},'w':'wwwwww','e':'eeeee'},'c':'xxxxx'}, true,
-   function(error, results) {
-          if (error) { 
-             sys.print('Error: ' + error + "\n");
-             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
-          }
-          else {
-            sys.puts(results.ok + "\n");
-          }
-   }
-);
-
-mdbm.setJSON('mdbmTest4', ["this","is","cool"], true,
-   function(error, results) {
-          if (error) { 
-             sys.print('Error: ' + error + "\n");
-             sys.puts(results.ErrorCode + ": " + results.ErrorMessage + "\n"); 
-          }
-          else {
-            sys.puts(results.ok + "\n");
-          }
-   }
-);
 
